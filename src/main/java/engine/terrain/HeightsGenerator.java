c93272396e7e3b69106cd66e9b1d8e97048a3643
Kajetan Lipensky <kajetanlipensky@gmail.com>
2017-11-10 15:38:58 +0100

    terrain loading

---
+++ src/main/java/engine/terrain/HeightsGenerator.java
@@ -0,0 +1,20 @@
+package engine.terrain;
+
+import java.util.Random;
+
+public class HeightsGenerator {
+
+    private static final float AMPLITUDE = 70f;
+
+    private Random random = new Random();
+    private int seed;
+
+    public HeightsGenerator() {
+        this.seed = random.nextInt(100000000);
+    }
+
+    public float generateHeight(int x, int z) {
+
+    }
+
+}
